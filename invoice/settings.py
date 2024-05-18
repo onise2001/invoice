9642f2095eebbfb2dfb11d944d9d578ea5cd306d
onise2001 <onise.babulashvili@yahoo.com>
2024-05-19 01:50:51 +0400

    Changed allowed hosts

--- invoice/settings.py
+++ invoice/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--068kgl^)q$oq0qs5$q+3naq_s+(wt0exsa318r041&6qja9n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
