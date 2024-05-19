99f77f2a7fe6c765a3bb303c0366e211f9bd787d
onise2001 <onise.babulashvili@yahoo.com>
2024-05-19 17:57:06 +0400

    Changed debug to False

--- invoice/settings.py
+++ invoice/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--068kgl^)q$oq0qs5$q+3naq_s+(wt0exsa318r041&6qja9n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -127,12 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = '/home/django/django_project/django_project/static'
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
