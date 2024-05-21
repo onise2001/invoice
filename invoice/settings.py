7bdb0de51a1cca53c753ff7da4c9319c4f60ef41
onise2001 <onise.babulashvili@yahoo.com>
2024-05-21 13:19:42 +0400

    Changed database for team 3

--- invoice/settings.py
+++ invoice/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,7 +88,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # },
-    'default': dj_database_url.parse('postgres://avnadmin:AVNS_otCx4MHhvl2ZzZNm-99@invoiceapi-invoiceapiteam1.k.aivencloud.com:28057/defaultdb?sslmode=require')
+    'default': dj_database_url.parse('postgres://koyeb-adm:nmW54lCuPZga@ep-dark-dawn-a2em1jb2.eu-central-1.pg.koyeb.app/koyebdb')
 }
 
 
